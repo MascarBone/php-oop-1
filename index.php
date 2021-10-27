25cc9acce109c04a1cc18c9aefb614cc92bba59c
MascarBone <simone.bonelli@gmail.com>
2021-10-27 16:05:07 +0200

    Output in index of an instance of movie

--- index.php
+++ index.php
@@ -2,7 +2,20 @@
 
 require_once __DIR__ . '/movie.php';
 
-
+$movies = [new Movie(
+    10001,
+    'Last Night in Soho',
+    'https://m.media-amazon.com/images/M/MV5BZjgwZDIwY2MtNGZlNy00NGRlLWFmNTgtOTBkZThjMDUwMGJhXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_FMjpg_UX1000_.jpg',
+    ['drama','horror','thriller'],
+    'english',
+    '04/11/2021',
+    118,
+    false,
+    'Una giovane ragazza, appassionata di design della moda, è misteriosamente in grado di entrare negli anni \'60.',
+    7,
+    ['Thomasin McKenzie', 'Anya Taylor-Joy', 'Matt Smith', 'Diana Rigg', 'Terence Stamp', 'Synnove Karlsen', 'James Phelps', 'Rita Tushingham', 'Oliver Phelps', 'Michael Jibson'],    
+),
+];
 
 
 ?>
@@ -12,10 +25,44 @@ require_once __DIR__ . '/movie.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>php-oop-1</title>
 </head>
 <body>
-    <h1><?php var_dump($obj); ?>
-    </h1>
+    
+    <main>
+        <?php foreach($movies as $movie) {?>
+            
+            <section>
+                <h2><?= $movie->getTitle(); ?></h2>
+
+                <div class="wrapper">               
+
+                    <div class="side side-left">
+                        <div class="poster">
+                            <img src="<?= $movie->getPosterPath(); ?>" alt="<?= $movie->getTitle(); ?>">
+                        </div>
+                    </div>
+                    <div class="side side-right">
+                        <p><?= $movie->getGenres(); ?></p>
+                        <p><?= $movie->getLanguage(); ?></p>
+                        <p><?= $movie->getReleaseDate(); ?></p>
+                        <p><?= $movie->getLength(); ?></p>
+                        <p><?= $movie->getAdult() ? 'For Adult' : 'Below 18'; ?></p>
+                        <p><?= $movie->getOverview(); ?></p>
+                        <p><?= $movie->getReview(); ?></p>
+                        <p><?= $movie->getCast(); ?></p>
+                    </div>
+                    
+                </div>
+            </section>
+            
+
+
+        <?php }; ?>
+            
+        
+    </main>
+
 </body>
 </html>
--- movie.php
+++ movie.php
@@ -3,10 +3,10 @@
 class Movie {
     public $id;
     public $title;
-    public $poster_path;
-    public $genre;
+    public $posterPath;
+    public $genres;
     public $language;
-    public $release_date;
+    public $releaseDate;
     public $length;
     public $adult;
     public $overview;
@@ -14,28 +14,28 @@ class Movie {
     public $cast;
 
     /**
-     * Undocumented function
+     * Construct to set the initial values of the instance
      *
      * @param int $_id                  ID of the movie
      * @param string $_title            Title of the movie         
-     * @param string $_poster_path      Path to the image of the movie
-     * @param array $_genre             List of the genres of the movie
+     * @param string $_posterPath      Path to the image of the movie
+     * @param array $_genres             List of the genres of the movie
      * @param string $_language         Language the movie will be proiected
-     * @param string $_release_date     Date of release of the movie
+     * @param string $_releaseDate     Date of release of the movie
      * @param int $_length              Duration in minute of the movie
      * @param bool $_adult              True or false if the film is for adults
      * @param string $_overview         Brief description of the plot
      * @param int $_review              A number between 1 and 10
      * @param array $_cast              List of acthors
      */
-    function __construct($_id,$_title,$_poster_path,$_genre,$_language,$_release_date,$_length,$_adult,$_overview,$_review,$_cast) {
+    function __construct($_id,$_title,$_posterPath,$_genres,$_language,$_releaseDate,$_length,$_adult,$_overview,$_review,$_cast) {
 
         $this->id = $_id;
         $this->title = $_title;
-        $this->poster_path = $_poster_path;
-        $this->genre = $_genre;
+        $this->posterPath = $_posterPath;
+        $this->genres = $_genres;
         $this->language = $_language;
-        $this->release_date = $_release_date;
+        $this->releaseDate = $_releaseDate;
         $this->length = $_length;
         $this->adult = $_adult;
         $this->overview = $_overview;
@@ -43,6 +43,116 @@ class Movie {
         $this->cast = $_cast;
     }
 
+    /**
+     * Get the id value
+     *
+     * @return int ID
+     */
+    function getId () {
+        return $this->id;
+    }
+    /**
+     * Get the title of the movie
+     *
+     * @return string Title
+     */
+    function getTitle () {
+        return $this->title;
+    }
+    /**
+     * Get the path to the poster
+     *
+     * @return string img-path
+     */
+    function getPosterPath () {
+        return $this->posterPath;
+    }
+    /**
+     * Get genres from an array of movies
+     *
+     * @return string list of genres
+     */
+    function getGenres () {
+        $list = '';
+        foreach($this->genres as $index => $genre)
+        {
+            if(count($this->genres)-1 == $index)
+            {
+                $list .= $genre;   
+                break;
+            }
+            $list .= $genre . ', ';   
+
+        }
+        return $list;
+    }
+    /**
+     * Get the language of the movie
+     *
+     * @return string language
+     */
+    function getLanguage () {
+        return $this->language;
+    }
+    /**
+     * Get the date of release of the movie
+     *
+     * @return string date
+     */
+    function getReleaseDate () {
+        return $this->releaseDate;
+    }
+    /**
+     * Get the duration of the movie in minutes
+     *
+     * @return int minutes
+     */
+    function getLength () {
+        return $this->length;
+    }
+    /**
+     * Get the info if the movie is for adult
+     *
+     * @return bool true or false
+     */
+    function getAdult () {
+        return $this->adult;
+    }
+    /**
+     * Get the plot of the movie
+     *
+     * @return string plot
+     */
+    function getOverview () {
+        return $this->overview;
+    }
+    /**
+     * Get the review between 1 an 10
+     *
+     * @return int review
+     */
+    function getReview () {
+        return $this->review;
+    }
+    /**
+     * Get the list of achtors
+     *
+     * @return array cast
+     */
+    function getCast () {
+        $list = '';
+        foreach($this->cast as $index => $achtor)
+        {
+            if(count($this->cast)-1 == $index)
+            {
+                $list .= $achtor;   
+                break;
+            }
+            $list .= $achtor . ', ';   
+
+        }
+        return $list;;
+    }
 }
 
 ?>
